dc98676e941f8255174f7b33eba32bad3045334e
Thirat958 <thirat.p@gmail.com>
2019-09-05 10:29:17 +0200

    nouvelle version: 1.16

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php
 
 // Mon projet
+echo "<h1>GITHUB</h1>";
+echo "<br>";
 echo "Mon projet v1";
 echo "<br>";
 echo "Hello";
 echo "<br>";
 echo "Mise à jour réalisée";
+echo "<br>";
+echo "<h1>GITHUB</h1>";
